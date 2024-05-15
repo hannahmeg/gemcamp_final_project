a4d6292f79fad9153be17f2adf67773a9cd5c188
hannahmeg <hannah_arcega@yahoo.com>
2024-05-15 21:49:38 +0800

    Validated user phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :image, ImageUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: { client: 0, admin: 1 }
-
-  mount_uploader :image, ImageUploader
+  validate :valid_phone_number
 
   attr_accessor :current_password
+
+  enum role: { client: 0, admin: 1 }
+
+  has_many :addresses
+  belongs_to :parent, class_name: 'User', optional: true
+  has_many :children, class_name: 'User', foreign_key: 'parent_id', dependent: :nullify, counter_cache: :children_members_count
+
+  private
+
+  def valid_phone_number
+    unless Phonelib.valid_for_country?(phone_number, 'PH')
+      errors.add(:phone_number, "Phone number should be valid.")
+    end
+  end
 end
