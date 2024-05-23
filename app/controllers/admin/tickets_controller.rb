4ae7411499b9cb7af5a4bf7f6246fdbc2deda384
hannahmeg <hannah_arcega@yahoo.com>
2024-05-23 17:08:06 +0800

    Refactored index method for ticket controller

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -2,31 +2,7 @@ class Admin::TicketsController < AdminController
 
   def index
     @tickets = Ticket.all
-
-    if params[:serial_number].present?
-      @tickets = @tickets.where(serial_number: params[:serial_number])
-    end
-
-    if params[:item_name].present?
-      @tickets = @tickets.joins(:item).where('items.name LIKE ?', "%#{params[:item_name]}%")
-    end
-
-    if params[:email].present?
-      @tickets = @tickets.joins(:user).where('LOWER(users.email) LIKE ?', "%#{params[:email].downcase}%")
-    end
-
-    if params[:state].present? && params[:state] != 'All'
-      @tickets = @tickets.where(state: params[:state])
-    end
-
-    if params[:start_date].present?
-      @tickets = @tickets.where('created_at >= ?', params[:start_date])
-    end
-
-    if params[:end_date].present?
-      @tickets = @tickets.where('created_at <= ?', params[:end_date])
-    end
-
+    filter_tickets(params)
     @tickets = @tickets.order(created_at: :desc)
   end
 
@@ -41,5 +17,14 @@ class Admin::TicketsController < AdminController
 
   def ticket_params
     params.require(:ticket).permit(:quantity)
+  end
+
+  def filter_tickets(params)
+    @tickets = @tickets.where(serial_number: params[:serial_number]) if params[:serial_number].present?
+    @tickets = @tickets.joins(:item).where('items.name LIKE ?', "%#{params[:item_name]}%") if params[:item_name].present?
+    @tickets = @tickets.joins(:user).where('LOWER(users.email) LIKE ?', "%#{params[:email].downcase}%") if params[:email].present?
+    @tickets = @tickets.where(state: params[:state]) if params[:state].present? && params[:state] != 'All'
+    @tickets = @tickets.where('created_at >= ?', params[:start_date]) if params[:start_date].present?
+    @tickets = @tickets.where('created_at <= ?', params[:end_date]) if params[:end_date].present?
   end
 end
