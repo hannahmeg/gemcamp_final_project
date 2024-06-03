4c852f0ae3989b01171d0c4b75f955233fa53163
hannahmeg <hannah_arcega@yahoo.com>
2024-06-03 16:25:02 +0800

    [Feature 33] Client Homepage view

--- app/controllers/client/homepage_controller.rb
+++ app/controllers/client/homepage_controller.rb
@@ -3,5 +3,7 @@ class Client::HomepageController < ClientController
   def index
     @banners = Banner.active.where("online_at <= ? AND offline_at >= ?", Time.current, Time.current)
     @news_tickers = NewsTicker.active.limit(5)
+    @winners = Winner.includes(:user).published
+    @items = Item.active.starting.limit(8).where("online_at <= ? AND offline_at >= ?", Time.current, Time.current)
   end
 end
